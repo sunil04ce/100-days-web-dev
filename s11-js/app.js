be6837644764ee8e5c434a155c89440c7dc67676
sunil parmar <sunil04ce@gmail.com>
2023-10-06 08:15:36 +0530

    use of array, object

---
+++ s11-js/app.js
@@ -0,0 +1,21 @@
+
+let age = 35;
+let userName = 'Jack';
+let hobbies = ['Sports', 'Reading', 'Walking'];
+alert(hobbies[1]);
+
+let job = {
+    title: 'Developer',
+    place: 'NY',
+    salary: 50000,
+};
+alert(job.title);
+
+// let greetingText = 'Hi, I\'m Jack!';
+// let age = 50;
+// alert(greetingText);
+// greetingText = "This is really Jack!";
+// alert(greetingText);
+// alert(age);
+// age = 51;
+// alert('Updated age : ' + age);
