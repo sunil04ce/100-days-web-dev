0d1b73fa88c5286ee2679fd9b72da79c5700f72c
sunil parmar <sunil04ce@gmail.com>
2023-10-15 23:42:18 +0530

    use of operators

--- s11-js/operations.js
+++ s11-js/operations.js
@@ -4,3 +4,27 @@ console.log(10 - 4);
 console.log(10 * 4);
 console.log(10 / 4);
 console.log(10 % 4);
+
+console.log((10 + 3 - 5) * 10);
+
+let result = (10 + 3 - 5) * 10;
+result = 10 * 4;
+result++;
+result--;
+result += 5;
+result -= 5;
+result /= 5;
+result *= 5;
+
+console.log(result);
+
+console.log('Jack' + ' ' + 'John');
+let a = 4 ** 3;
+console.log(a);
+
+let userName = 'Jack';
+console.log(userName.length);
+console.log(userName.toUpperCase());
+
+let hobbies = ['Sports', 'Gaming'];
+console.log(hobbies.length);
