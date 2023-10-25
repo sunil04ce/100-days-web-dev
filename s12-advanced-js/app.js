d791cce48dae90a2b1aa583d253385d1e83197ee
sunil parmar <sunil04ce@gmail.com>
2023-10-25 23:53:42 +0530

    insert and delete html element

--- s12-advanced-js/app.js
+++ s12-advanced-js/app.js
@@ -1,11 +1,23 @@
 // console.dir(document);
 // document.body.children[1].children[0].href = 'https://google.com';
 
-let anchorElement = document.getElementById('external-link');
-anchorElement.innerHTML = 'google link';
-anchorElement.href = 'https://google.com';
+let anchorElement = document.getElementById("external-link");
+anchorElement.innerHTML = "google link";
+anchorElement.href = "https://google.com";
 
-anchorElement = document.querySelector('#external-link');
+anchorElement = document.querySelector("#external-link");
 // anchorElement = document.querySelector('a');
 // anchorElement = document.querySelector('p a');
-anchorElement.href = 'https://academind.com';
+anchorElement.href = "https://academind.com";
+
+let newAnchorElement = document.createElement("a");
+newAnchorElement.href = "https://google.com";
+newAnchorElement.textContent = "this leads to google";
+
+let fpara = document.querySelector("p");
+
+fpara.append(newAnchorElement);
+
+let fh1Element = document.querySelector("h1");
+fh1Element.remove();
+// fh1Element.parentElement.removeChild(fh1Element);
